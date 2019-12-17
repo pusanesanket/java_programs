10522d78c3c1fd7787cc6ed2c50d73697adde3d8
pusanesanket <pusanesanket@gmail.com>
2019-12-17 18:50:18 +0000

    Subarraywith given sum

---
+++ java programming/src/SubarrayWithGivenSum.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+public class SubarrayWithGivenSum {
+	public static int[] getSubarrayLocationWithGivenSum(int noOfTestCases, int size, LinkedList<Integer> inputlist, int sum)
+	{
+		int total = 0 ;
+		for(int i=0;i<size;i++) {
+			total= inputlist.get(i);
+			if(total == sum)
+					return new int[] {i+1,i+1};
+			for(int j=i+1;j<size && total<sum;j++) {
+				total = total+inputlist.get(j);
+				if(total == sum)
+					return new int[] {i+1,j+1};
+			}
+		}
+		return new int[] {-1};
+	}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int noOfTestCases = Integer.parseInt(sc.nextLine());
+		for(int j=0;j< noOfTestCases;j++) {
+		String[] sizeOfArraySum = sc.nextLine().split(" ");
+		
+		int size = Integer.parseInt(sizeOfArraySum[0]);
+		int sum = Integer.parseInt(sizeOfArraySum[1]);
+		
+		String list = sc.nextLine();
+		LinkedList<Integer> inputlist = new LinkedList<Integer>();
+		for(String number:list.split(" "))
+			inputlist.add(Integer.parseInt(number));
+		
+//		Collections.sort(inputlist);
+		int[] output = getSubarrayLocationWithGivenSum(noOfTestCases,size,inputlist,sum);
+		if(output[0] > -1)
+			System.out.println(output[0]+" "+output[1]);
+		else
+			System.out.println(output[0]);
+		}
+	}
+}
